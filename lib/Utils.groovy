f2421a1c5fa17771fbb0bbb3ffff3c4b4dcbb2bb
cyx <sukhikh@moscollector.ru>
2021-10-26 13:21:46 +0300

    wip-1

--- lib/Utils.groovy
+++ lib/Utils.groovy
@@ -17,22 +17,6 @@ def TimeNow() {
     LocalDateTime t = LocalDateTime.now()
     return t
 }
-def getFilePath(String path) {
-    IStepExecutor steps = ContextRegistry.getContext().getStepExecutor()
-
-    def env = steps.env()
-
-    String nodeName = env.NODE_NAME
-    if (nodeName == null) {
-        steps.error 'Переменная среды NODE_NAME не задана. Запуск вне node или без agent?'
-    }
-
-    if (nodeName == 'master') {
-        return new FilePath(new File(path))
-        } else {
-        return new FilePath(Jenkins.getInstanceOrNull().getComputer(nodeName).getChannel(), path)
-    }
-}
 
 def getPathSize(myFilePath) {
     pt = getFilePath(myFilePath)
