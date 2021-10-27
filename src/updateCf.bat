1c964f68d20101fdfe6fbd3484075c08187ee270
cyx <sukhikh@moscollector.ru>
2021-10-27 17:09:35 +0300

    changed updateCf.bat

--- src/updateCf.bat
+++ src/updateCf.bat
@@ -8,7 +8,7 @@ if !%3!==!! (
 	echo ТРЕБУЕТСЯ 3 параметра - строка соединения, код разрешения и имя файла лога
 	exit 5
 )
-echo 1cv8.exe" config /IBConnectionString %1 /UpdateDBCfg /uc %2 /out %3 /DisableStartupMessages /DisableStartupDialogs
-"%root1c%\%ver1c%\bin\1cv8.exe" config /IBConnectionString %1 /UpdateDBCfg /uc%2 /out %3 /DisableStartupMessages /DisableStartupDialogs
+echo 1cv8.exe" config /IBConnectionString %1 /UC %2 /UpdateDBCfg -Server  /DisableStartupMessages /DisableStartupDialogs /out %3
+"%root1c%\%ver1c%\bin\1cv8.exe" config /IBConnectionString %1 /UC %2 /UpdateDBCfg -Server  /DisableStartupMessages /DisableStartupDialogs /out %3
 
 exit %ERRORLEVEL%
