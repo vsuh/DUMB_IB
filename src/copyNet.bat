6102efa776ad15ea7628ad1437c01f6a69d4704e
cyx <sukhikh@moscollector.ru>
2021-11-24 17:01:40 +0300

    copy.bat updated

--- src/copyNet.bat
+++ src/copyNet.bat
@@ -30,13 +30,30 @@ if not exist %2 (
 	echo НЕ УДАЛОСЬ ПОДКЛЮЧИТЬ ДИСК %2
         exit 5
 )
+
 Set dest=%2\%~nx1
+Set root=%~dp1
+Set nmpt=%~n1
+Set nmpt=%nmpt:~10,%
 if exist %dest% del %dest%
 
 echo xcopy /Y /Z /L %fn% %dest%
 cd.>>%dest%
 ::echo f|xcopy /V /I /Y /Z /F /R  %fn% %dest%
 xcopy /Y /Z /L %fn% %dest%
+
+exit %ERRORLEVEL%
+
+:: удаление старых файлов выгрузок
+FOR %%A in (%root%\*.dt) DO (
+	FOR /F %%K IN ("%%~tA") DO (
+		FOR /F "delims=. tokens=1-3" %%X in ("%%K") DO SET /a fd=%%Z+%%Y*12*40+%%X
+		SET /a df=!td!-!fd!
+		IF !df! GEQ %kep% del %%A
+		)
+	)
+
+
 
 exit %ERRORLEVEL%
 
